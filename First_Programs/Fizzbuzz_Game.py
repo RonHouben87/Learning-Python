2804a7dc93495e09a10d2da95763a9d249c971a0
Ron Houben <houbenron@hotmail.com>
2026-02-12 09:25:08 +0100

    Update Fizzbuzz_Game.py

--- First_Programs/Fizzbuzz_Game.py
+++ First_Programs/Fizzbuzz_Game.py
@@ -1,3 +1,30 @@
+#_____OPTION 1_____
+
+print("Welcome to FizzBuzz!")
+
+# Declaring the function
+def fizzbuzz(n):
+    if (n % 3 == 0 and n % 7 == 0):
+        return "FizzBuzz"
+    elif n % 3 == 0:
+        return "Fizz"
+    elif n % 7 == 0:
+        return "Buzz"
+    elif "3" in str(n):   # check for number 3 in the str(int())
+        return "Almost Fizz"
+    else:
+        return n
+        
+# Input from user
+limit = int(input())
+
+# Play FizzBuzz
+for i in range(1,limit + 1):
+    result = fizzbuzz(i)
+    print(result)
+
+#_____OPTION 2_____
+
 print("Welcome to FizzBuzz!")
 
 def fizzbuzz(number):
